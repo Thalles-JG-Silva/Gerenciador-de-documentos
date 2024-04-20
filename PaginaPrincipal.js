0c4404f94f48795a9805f5ca1ccbe1e52a97e219
Thalles Silva <98830931+Thalles-JG-Silva@users.noreply.github.com>
2024-04-20 15:36:07 -0300

    Mudei o nome do arquivo css, exclui os arquivos js para unificação já que nenhum tinha efeitos praticos.

--- PaginaPrincipal.js
+++
@@ -1,6 +0,0 @@
-const hamburguer = document.querySelector(".menu-hamburguer");
-
-const nav = document.querySelector("nav");
-
-hamburguer.addEventListener("click", () =>nav.classList.toggle("active"));
-''
