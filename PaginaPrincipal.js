5581bfcdedc32435f27b9b89822f164122691532
amanda15900 <166137073+amanda15900@users.noreply.github.com>
2024-04-21 20:11:05 -0300

    atualizado

--- PaginaPrincipal.js
+++ PaginaPrincipal.js
@@ -41,3 +41,77 @@
                 }
             });
         });
+/* Abre as paginas dos botoes */
+function openpage(pageUrl) {
+    window.open(pageUrl, '_self');
+}
+// botao Excluir// 
+function excluirCertificado(event) {
+    // Confirmar se o usuário realmente deseja excluir o certificado
+    if (confirm("Tem certeza de que deseja excluir este certificado?")) {
+        // Obter a referência ao elemento pai do ícone de exclusão (que é a célula da tabela)
+        var tableCell = event.target.closest("td");
+        
+        // Obter a referência à linha (tr) que contém o certificado a ser excluído
+        var tableRow = tableCell.closest("tr");
+        
+        // Remover a linha (tr) da tabela
+        tableRow.remove();
+        
+        // Exibir uma mensagem de confirmação
+        alert("Certificado excluído com sucesso!");
+    }
+}
+// imprimir//
+function imprimirPDF(pdfFileName) {
+    // Criar um elemento iframe
+    var iframe = document.createElement('iframe');
+    
+    // Definir o atributo src do iframe como o nome do arquivo PDF
+    iframe.src = pdfFileName;
+    
+    // Definir o estilo do iframe para não ser exibido na página
+    iframe.style.display = 'none';
+    
+    // Adicionar o iframe ao corpo do documento
+    document.body.appendChild(iframe);
+    
+    // Esperar um curto período de tempo para o PDF carregar
+    setTimeout(function() {
+        // Chamar a função de impressão do iframe
+        iframe.contentWindow.print();
+    }, 1000);
+}
+
+/*Pesquisa */
+function searchCertificates() {
+    // Pegar o valor digitado na barra de pesquisa
+    var input = document.getElementById("searchInput");
+    var filter = input.value.toUpperCase();
+
+    // Pegar a tabela e as linhas da tabela
+    var table = document.querySelector(".panel-body table");
+    var rows = table.getElementsByTagName("tr");
+
+    // Loop através de todas as linhas da tabela e ocultar aquelas que não correspondem à pesquisa
+    for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        var found = false;
+        for (var j = 0; j < cells.length && !found; j++) {
+            var cell = cells[j];
+            if (cell && cell.textContent) { // Verifica se a célula e seu conteúdo não são nulos
+                var textValue = cell.textContent.toUpperCase();
+                if (textValue.indexOf(filter) > -1) {
+                    found = true;
+                }
+            }
+        }
+        // Exibir ou ocultar a linha da tabela com base no resultado da pesquisa
+        if (found) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+
