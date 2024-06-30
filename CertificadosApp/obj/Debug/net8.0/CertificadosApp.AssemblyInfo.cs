80c21d376c269087058ced88e982bb4ed324b6a6
Thalles Silva <98830931+Thalles-JG-Silva@users.noreply.github.com>
2024-06-30 11:55:01 -0300

    Script banco de dados

--- CertificadosApp/obj/Debug/net8.0/CertificadosApp.AssemblyInfo.cs
+++ CertificadosApp/obj/Debug/net8.0/CertificadosApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertificadosApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7c9d9498e433cfd2cdfd097b40259ebbb7b03c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907e47422930a2b6fc0cbad528899060f5279a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertificadosApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertificadosApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SQLQueryCertificadosDB.sql
+++ SQLQueryCertificadosDB.sql
@@ -1,143 +1,45 @@
-USE [master]
-GO
-/****** Object:  Database [CertificadosDB]    Script Date: 30/06/2024 11:24:46 ******/
-CREATE DATABASE [CertificadosDB]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'CertificadosDB', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\CertificadosDB.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'CertificadosDB_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\CertificadosDB_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
-GO
-ALTER DATABASE [CertificadosDB] SET COMPATIBILITY_LEVEL = 150
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [CertificadosDB].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [CertificadosDB] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET AUTO_CLOSE ON 
-GO
-ALTER DATABASE [CertificadosDB] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [CertificadosDB] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [CertificadosDB] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET  ENABLE_BROKER 
-GO
-ALTER DATABASE [CertificadosDB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [CertificadosDB] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [CertificadosDB] SET  MULTI_USER 
-GO
-ALTER DATABASE [CertificadosDB] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [CertificadosDB] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [CertificadosDB] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [CertificadosDB] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-ALTER DATABASE [CertificadosDB] SET DELAYED_DURABILITY = DISABLED 
-GO
-ALTER DATABASE [CertificadosDB] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-ALTER DATABASE [CertificadosDB] SET QUERY_STORE = OFF
-GO
-USE [CertificadosDB]
-GO
-/****** Object:  Table [dbo].[Certificados]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Certificados](
-	[CertificadoID] [int] IDENTITY(1,1) NOT NULL,
-	[Titulo] [nvarchar](255) NOT NULL,
-	[Tipo] [nvarchar](50) NOT NULL,
-	[Horas] [int] NOT NULL,
-	[Data] [date] NOT NULL,
-	[Participante] [nvarchar](255) NOT NULL,
-	[Participacao] [nvarchar](50) NOT NULL,
-	[Excluido] [bit] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[CertificadoID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Usuarios]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Usuarios](
-	[UsuarioID] [int] IDENTITY(1,1) NOT NULL,
-	[Nome] [nvarchar](255) NOT NULL,
-	[NomeUsuario] [nvarchar](255) NOT NULL,
-	[Email] [nvarchar](255) NOT NULL,
-	[Senha] [nvarchar](255) NOT NULL,
-	[DataCadastro] [datetime] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[UsuarioID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[NomeUsuario] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[Email] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Certificados] ADD  DEFAULT ((0)) FOR [Excluido]
-GO
-ALTER TABLE [dbo].[Usuarios] ADD  DEFAULT (getdate()) FOR [DataCadastro]
-GO
-/****** Object:  StoredProcedure [dbo].[AutenticarUsuario]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
+-- Cria o banco de dados 'CertificadosDB'
+CREATE DATABASE CertificadosDB
 GO
 
--- Procedure para autenticar um usuário
-CREATE PROCEDURE [dbo].[AutenticarUsuario]
+-- Seleciona o banco de dados 'CertificadosDB' para uso
+USE CertificadosDB
+GO
+
+-- Cria a tabela 'Certificados'
+CREATE TABLE dbo.Certificados(
+    CertificadoID int IDENTITY(1,1) NOT NULL, -- Coluna 'CertificadoID' com incremento automático
+    Titulo nvarchar(255) NOT NULL,            -- Coluna 'Titulo' do tipo texto variável até 255 caracteres
+    Tipo nvarchar(50) NOT NULL,               -- Coluna 'Tipo' do tipo texto variável até 50 caracteres
+    Horas int NOT NULL,                       -- Coluna 'Horas' do tipo inteiro
+    Data date NOT NULL,                       -- Coluna 'Data' do tipo data
+    Participante nvarchar(255) NOT NULL,      -- Coluna 'Participante' do tipo texto variável até 255 caracteres
+    Participacao nvarchar(50) NOT NULL,       -- Coluna 'Participacao' do tipo texto variável até 50 caracteres
+    Excluido bit NOT NULL,                    -- Coluna 'Excluido' do tipo bit (booleano)
+    PRIMARY KEY (CertificadoID)               -- Define 'CertificadoID' como chave primária
+)
+GO
+
+-- Cria a tabela 'Usuarios'
+CREATE TABLE dbo.Usuarios(
+    UsuarioID int IDENTITY(1,1) NOT NULL,     -- Coluna 'UsuarioID' com incremento automático
+    Nome nvarchar(255) NOT NULL,              -- Coluna 'Nome' do tipo texto variável até 255 caracteres
+    NomeUsuario nvarchar(255) NOT NULL,       -- Coluna 'NomeUsuario' do tipo texto variável até 255 caracteres
+    Email nvarchar(255) NOT NULL,             -- Coluna 'Email' do tipo texto variável até 255 caracteres
+    Senha nvarchar(255) NOT NULL,             -- Coluna 'Senha' do tipo texto variável até 255 caracteres
+    DataCadastro datetime NOT NULL DEFAULT (getdate()), -- Coluna 'DataCadastro' do tipo datetime com valor padrão como data atual
+    PRIMARY KEY (UsuarioID),                  -- Define 'UsuarioID' como chave primária
+    UNIQUE (NomeUsuario),                     -- Define 'NomeUsuario' como único
+    UNIQUE (Email)                            -- Define 'Email' como único
+)
+GO
+
+-- Adiciona um valor padrão de 0 para a coluna 'Excluido' na tabela 'Certificados'
+ALTER TABLE dbo.Certificados ADD DEFAULT ((0)) FOR Excluido
+GO
+
+-- Cria a stored procedure 'AutenticarUsuario' para autenticar um usuário
+CREATE PROCEDURE dbo.AutenticarUsuario
     @NomeUsuario NVARCHAR(255),
     @Senha NVARCHAR(255)
 AS
@@ -147,14 +49,9 @@ BEGIN
     WHERE NomeUsuario = @NomeUsuario AND Senha = @Senha;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[CriarCertificado]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Procedure para criar um novo certificado
-CREATE PROCEDURE [dbo].[CriarCertificado]
+-- Cria a stored procedure 'CriarCertificado' para criar um novo certificado
+CREATE PROCEDURE dbo.CriarCertificado
     @Titulo NVARCHAR(255),
     @Tipo NVARCHAR(50),
     @Horas INT,
@@ -167,14 +64,9 @@ BEGIN
     VALUES (@Titulo, @Tipo, @Horas, @Data, @Participante, @Participacao, 0);
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[CriarUsuario]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Procedure para criar um novo usuário
-CREATE PROCEDURE [dbo].[CriarUsuario]
+-- Cria a stored procedure 'CriarUsuario' para criar um novo usuário
+CREATE PROCEDURE dbo.CriarUsuario
     @Nome NVARCHAR(255),
     @NomeUsuario NVARCHAR(255),
     @Email NVARCHAR(255),
@@ -185,14 +77,9 @@ BEGIN
     VALUES (@Nome, @NomeUsuario, @Email, @Senha);
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[EditarCertificado]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Procedure para editar um certificado existente
-CREATE PROCEDURE [dbo].[EditarCertificado]
+-- Cria a stored procedure 'EditarCertificado' para editar um certificado existente
+CREATE PROCEDURE dbo.EditarCertificado
     @CertificadoID INT,
     @Titulo NVARCHAR(255),
     @Tipo NVARCHAR(50),
@@ -212,14 +99,9 @@ BEGIN
     WHERE CertificadoID = @CertificadoID;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[ExcluirCertificado]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Procedure para excluir (mover para a lixeira) um certificado
-CREATE PROCEDURE [dbo].[ExcluirCertificado]
+-- Cria a stored procedure 'ExcluirCertificado' para mover um certificado para a lixeira
+CREATE PROCEDURE dbo.ExcluirCertificado
     @CertificadoID INT
 AS
 BEGIN
@@ -228,14 +110,9 @@ BEGIN
     WHERE CertificadoID = @CertificadoID;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[ExcluirPermanentementeCertificado]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Procedure para excluir permanentemente um certificado
-CREATE PROCEDURE [dbo].[ExcluirPermanentementeCertificado]
+-- Cria a stored procedure 'ExcluirPermanentementeCertificado' para excluir permanentemente um certificado
+CREATE PROCEDURE dbo.ExcluirPermanentementeCertificado
     @CertificadoID INT
 AS
 BEGIN
@@ -243,14 +120,9 @@ BEGIN
     WHERE CertificadoID = @CertificadoID;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[RestaurarCertificado]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Procedure para restaurar um certificado da lixeira
-CREATE PROCEDURE [dbo].[RestaurarCertificado]
+-- Cria a stored procedure 'RestaurarCertificado' para restaurar um certificado da lixeira
+CREATE PROCEDURE dbo.RestaurarCertificado
     @CertificadoID INT
 AS
 BEGIN
@@ -259,48 +131,29 @@ BEGIN
     WHERE CertificadoID = @CertificadoID;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[VisualizarCertificados]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Visualizar certificados (incluindo lixeira)
-CREATE PROCEDURE [dbo].[VisualizarCertificados]
+-- Cria a stored procedure 'VisualizarCertificados' para visualizar todos os certificados
+CREATE PROCEDURE dbo.VisualizarCertificados
 AS
 BEGIN
     SELECT * FROM Certificados;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[VisualizarCertificadosAtivos]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Visualizar certificados não excluídos
-CREATE PROCEDURE [dbo].[VisualizarCertificadosAtivos]
+-- Cria a stored procedure 'VisualizarCertificadosAtivos' para visualizar certificados não excluídos
+CREATE PROCEDURE dbo.VisualizarCertificadosAtivos
 AS
 BEGIN
     SELECT * FROM Certificados
     WHERE Excluido = 0;
 END;
 GO
-/****** Object:  StoredProcedure [dbo].[VisualizarCertificadosExcluidos]    Script Date: 30/06/2024 11:24:47 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 
--- Visualizar certificados na lixeira
-CREATE PROCEDURE [dbo].[VisualizarCertificadosExcluidos]
+-- Cria a stored procedure 'VisualizarCertificadosExcluidos' para visualizar certificados excluídos (na lixeira)
+CREATE PROCEDURE dbo.VisualizarCertificadosExcluidos
 AS
 BEGIN
     SELECT * FROM Certificados
     WHERE Excluido = 1;
 END;
-GO
-USE [master]
-GO
-ALTER DATABASE [CertificadosDB] SET  READ_WRITE 
 GO
